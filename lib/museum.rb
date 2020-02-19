21f853c14b80dc63fc6b00159045f61514603759
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-19 11:00:49 -0700

    Attempted to make test it can return patrons by exhibit interest pass

--- lib/museum.rb
+++ lib/museum.rb
@@ -23,4 +23,19 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+  def patrons_by_exhibit_interest
+    exhibit_interest = {}
+    @patrons.find_all do |patron|
+      patron.interests == @exhibits.each do |exhibit|
+        exhibit.name
+        if exhibit_interest.include?(exhibit) && patron.interests == exhibit.name
+          exhibit_interest[exhibit] = exhibit_interest[exhibit] << patron
+        else
+          exhibit_interest[exhibit] = [patron]
+        end
+      end
+    end
+    exhibit_interest
+  end
 end
